087429fa46a119f4c5623b83f1074a9e7f202907
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-16 13:58:33 -0400

    created album data

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const albums = [
+  {
+    image: "/images/hitn-artwork.jpg",
+    name: 'I Wish There Were More Hours in the Night',
+    songs: ['I Wish There Were More Hours in the Night']
+  },
+  {
+    image: "/images/vanilla-cola-artwork.jpeg",
+    name: 'Vanilla Cola',
+    songs: ['Vanilla Cola']
+  },
+  {
+    image: "/images/lofi-1-artwork.jpg",
+    name: 'Lofi, Vol. 1',
+    songs: ['U Are', 'Wait, Take My Picture', "You're Cute", 'Her Garden', 'The Cafe', 'She Likes to Be Sad', 'Pineapple', 'Roads', 'Dreamless', 'Beacon//The Beginning (Bonus Track)']
+  },
+  {
+    image: "/images/4a4a4a-artwork.jpg",
+    name: '4A4A4A',
+    songs: ['4A4A4A']
+  },
+  {
+    image: "/images/roads-artwork.jpg",
+    name: 'Roads',
+    songs: ['Roads']
+  },
+  {
+    image: "/images/ds-artwork.jpg",
+    name: 'Decorating Silence',
+    songs: ['Coffee Shop Loops', "80's.Wav", 'Air Castle', 'Seaside Rain', 'To Silence']
+  }
+]
