25bf9f18a05f6804c75b033bf49bf41a002ee166
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-17 13:38:25 -0400

    css added to cleanly display data

--- js/data.js
+++ js/data.js
@@ -1,32 +1,38 @@
 const albums = [
   {
     image: "/images/hitn-artwork.jpg",
-    name: 'I Wish There Were More Hours in the Night',
+    name: 'I Wish There Were More Hours in the Night - Single',
+    date: 'September 6, 2018',
     songs: ['I Wish There Were More Hours in the Night']
   },
   {
     image: "/images/vanilla-cola-artwork.jpeg",
-    name: 'Vanilla Cola',
+    name: 'Vanilla Cola - Single',
+    date: 'July 25, 2018',
     songs: ['Vanilla Cola']
   },
   {
     image: "/images/lofi-1-artwork.jpg",
     name: 'Lofi, Vol. 1',
+    date: 'June 15, 2018',
     songs: ['U Are', 'Wait, Take My Picture', "You're Cute", 'Her Garden', 'The Cafe', 'She Likes to Be Sad', 'Pineapple', 'Roads', 'Dreamless', 'Beacon//The Beginning (Bonus Track)']
   },
   {
     image: "/images/4a4a4a-artwork.jpg",
-    name: '4A4A4A',
+    name: '4A4A4A - Single',
+    date: 'May 4, 2018',
     songs: ['4A4A4A']
   },
   {
     image: "/images/roads-artwork.jpg",
-    name: 'Roads',
+    name: 'Roads - Single',
+    date: 'April 20, 2018',
     songs: ['Roads']
   },
   {
     image: "/images/ds-artwork.jpg",
-    name: 'Decorating Silence',
+    name: 'Decorating Silence - EP',
+    date: 'October 20, 2017',
     songs: ['Coffee Shop Loops', "80's.Wav", 'Air Castle', 'Seaside Rain', 'To Silence']
   }
 ]
