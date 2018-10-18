84ef467bd4499e99bad1c89867a5ba5b9d73b977
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-18 16:47:48 -0400

    created sreaming links with clipart

--- js/data.js
+++ js/data.js
@@ -3,36 +3,54 @@ const albums = [
     image: "/images/hitn-artwork.jpg",
     name: 'I Wish There Were More Hours in the Night - Single',
     date: 'September 6, 2018',
-    songs: ['I Wish There Were More Hours in the Night']
+    songs: ['I Wish There Were More Hours in the Night'],
+    spotify: "https://open.spotify.com/album/1h81hhIIZrqnqYt0a20Y7Z?si=BDN-T0lTRV6-B0cMNJgcFg",
+    itunes: "https://itunes.apple.com/us/album/i-wish-there-were-more-hours-in-the-night-single/1434880840",
+    youtube: ""
   },
   {
     image: "/images/vanilla-cola-artwork.jpeg",
     name: 'Vanilla Cola - Single',
     date: 'July 25, 2018',
-    songs: ['Vanilla Cola']
+    songs: ['Vanilla Cola'],
+    spotify: "https://open.spotify.com/album/6CMbOQf0OejaZUkMgDNlaB?si=sUjORi5cTcKsmUFEgjzvEQ",
+    itunes: "https://itunes.apple.com/us/album/vanilla-cola-single/1417092367",
+    youtube: ""
   },
   {
     image: "/images/lofi-1-artwork.jpg",
     name: 'Lofi, Vol. 1',
     date: 'June 15, 2018',
-    songs: ['U Are', 'Wait, Take My Picture', "You're Cute", 'Her Garden', 'The Cafe', 'She Likes to Be Sad', 'Pineapple', 'Roads', 'Dreamless', 'Beacon//The Beginning (Bonus Track)']
+    songs: ['U Are', 'Wait, Take My Picture', "You're Cute", 'Her Garden', 'The Cafe', 'She Likes to Be Sad', 'Pineapple', 'Roads', 'Dreamless', 'Beacon//The Beginning (Bonus Track)'],
+    spotify: "https://open.spotify.com/album/0v3nEf6d3R7F5ijTpx5F4i?si=I4OKUxsfT1-BYRznOCf7uw",
+    itunes: "https://itunes.apple.com/us/album/lofi-vol-1/1394591976",
+    youtube: ""
   },
   {
     image: "/images/4a4a4a-artwork.jpg",
     name: '4A4A4A - Single',
     date: 'May 4, 2018',
-    songs: ['4A4A4A']
+    songs: ['4A4A4A'],
+    spotify: "https://open.spotify.com/album/0K6kRXdwOHC8NznS6lhvNR?si=UQx4IquqTuqgRVlxK6691w",
+    itunes: "https://itunes.apple.com/us/album/4a4a4a-single/1381044012",
+    youtube: ""
   },
   {
     image: "/images/roads-artwork.jpg",
     name: 'Roads - Single',
     date: 'April 20, 2018',
-    songs: ['Roads']
+    songs: ['Roads'],
+    spotify: "https://open.spotify.com/album/66JLvVU3JXcAlw6Z5mZLwd?si=53v3S7eJScOipF-GaS_ePQ",
+    itunes: "https://itunes.apple.com/us/album/roads-single/1372242040",
+    youtube: ""
   },
   {
     image: "/images/ds-artwork.jpg",
     name: 'Decorating Silence - EP',
     date: 'October 20, 2017',
-    songs: ['Coffee Shop Loops', "80's.Wav", 'Air Castle', 'Seaside Rain', 'To Silence']
+    songs: ['Coffee Shop Loops', "80's.Wav", 'Air Castle', 'Seaside Rain', 'To Silence'],
+    spotify: "https://open.spotify.com/album/6fbImGFgZe82tSpPqWE32b?si=q46mspXuTo2qSr_HSSR2Sg",
+    itunes: "https://itunes.apple.com/us/album/decorating-silence-ep/1311728765",
+    youtube: ""
   }
 ]
