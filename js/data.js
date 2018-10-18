acc560d317c96654cb8e30c395eff90caad8a657
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-18 17:10:34 -0400

    added youtube links

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ const albums = [
     songs: ['I Wish There Were More Hours in the Night'],
     spotify: "https://open.spotify.com/album/1h81hhIIZrqnqYt0a20Y7Z?si=BDN-T0lTRV6-B0cMNJgcFg",
     itunes: "https://itunes.apple.com/us/album/i-wish-there-were-more-hours-in-the-night-single/1434880840",
-    youtube: ""
+    youtube: "https://www.youtube.com/watch?v=CfEuCrAD5vY"
   },
   {
     image: "/images/vanilla-cola-artwork.jpeg",
@@ -15,7 +15,7 @@ const albums = [
     songs: ['Vanilla Cola'],
     spotify: "https://open.spotify.com/album/6CMbOQf0OejaZUkMgDNlaB?si=sUjORi5cTcKsmUFEgjzvEQ",
     itunes: "https://itunes.apple.com/us/album/vanilla-cola-single/1417092367",
-    youtube: ""
+    youtube: "https://www.youtube.com/watch?v=RE5iag_1ELw"
   },
   {
     image: "/images/lofi-1-artwork.jpg",
@@ -24,7 +24,7 @@ const albums = [
     songs: ['U Are', 'Wait, Take My Picture', "You're Cute", 'Her Garden', 'The Cafe', 'She Likes to Be Sad', 'Pineapple', 'Roads', 'Dreamless', 'Beacon//The Beginning (Bonus Track)'],
     spotify: "https://open.spotify.com/album/0v3nEf6d3R7F5ijTpx5F4i?si=I4OKUxsfT1-BYRznOCf7uw",
     itunes: "https://itunes.apple.com/us/album/lofi-vol-1/1394591976",
-    youtube: ""
+    youtube: "https://www.youtube.com/playlist?list=OLAK5uy_lnHZmcj68G-WaW98Y3pGfmbFHH6RdRnd4"
   },
   {
     image: "/images/4a4a4a-artwork.jpg",
@@ -33,7 +33,7 @@ const albums = [
     songs: ['4A4A4A'],
     spotify: "https://open.spotify.com/album/0K6kRXdwOHC8NznS6lhvNR?si=UQx4IquqTuqgRVlxK6691w",
     itunes: "https://itunes.apple.com/us/album/4a4a4a-single/1381044012",
-    youtube: ""
+    youtube: "https://www.youtube.com/watch?v=yBgheV7Meyk"
   },
   {
     image: "/images/roads-artwork.jpg",
@@ -42,7 +42,7 @@ const albums = [
     songs: ['Roads'],
     spotify: "https://open.spotify.com/album/66JLvVU3JXcAlw6Z5mZLwd?si=53v3S7eJScOipF-GaS_ePQ",
     itunes: "https://itunes.apple.com/us/album/roads-single/1372242040",
-    youtube: ""
+    youtube: "https://www.youtube.com/watch?v=QR9kNJ38MCI"
   },
   {
     image: "/images/ds-artwork.jpg",
@@ -51,6 +51,6 @@ const albums = [
     songs: ['Coffee Shop Loops', "80's.Wav", 'Air Castle', 'Seaside Rain', 'To Silence'],
     spotify: "https://open.spotify.com/album/6fbImGFgZe82tSpPqWE32b?si=q46mspXuTo2qSr_HSSR2Sg",
     itunes: "https://itunes.apple.com/us/album/decorating-silence-ep/1311728765",
-    youtube: ""
+    youtube: "https://www.youtube.com/playlist?list=OLAK5uy_kPqbNm4rcuQkazG8kxaFVOHBk66hnoixE"
   }
 ]
